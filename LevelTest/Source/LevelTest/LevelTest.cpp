8f4c6f4da898255cfad4611a5035154265cd2970
ByungKwon Hwang <noaenix@gmail.com>
2026-03-19 14:29:49 +0900

    refactoring

--- LevelTest/Source/LevelTest/LevelTest.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LevelTest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LevelTest, "LevelTest" );
-
-DEFINE_LOG_CATEGORY(LogLevelTest)
